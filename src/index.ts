bf77e3a65e8b9715278695fdafd93386c05cb5f7
LinkunGao <gaolinkun123@gmail.com>
2022-10-26 14:33:48 +0800

    update nrrd tools GUI layout

--- src/index.ts
+++ src/index.ts
@@ -33,8 +33,10 @@ import {
 
 import "./css/style.css";
 
+export const REVISION = "v1.12.2";
+
 console.log(
-  "%cCopper3D Visualisation %cBeta:v1.12.1",
+  "%cCopper3D Visualisation %cBeta:v1.12.2",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
