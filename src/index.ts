4a38a463b55ca7d3fab9261b824686291db1a033
LinkunGao <gaolinkun123@gmail.com>
2022-11-14 22:32:06 +0800

    add corsshair function

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,10 @@ import {
 
 import "./css/style.css";
 
-export const REVISION = "v1.12.13";
+export const REVISION = "v1.12.14";
 
 console.log(
-  "%cCopper3D Visualisation %cBeta:v1.12.13",
+  "%cCopper3D Visualisation %cBeta:v1.12.14",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
