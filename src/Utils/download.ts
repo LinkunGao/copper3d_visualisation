9b2f634c03bcbc5af7436b7406ae01d1b84201a3
LinkunGao <gaolinkun123@gmail.com>
2023-01-18 11:32:15 +1300

    file-saver should use require not import when build a package

--- src/Utils/download.ts
+++ src/Utils/download.ts
@@ -1,8 +1,8 @@
-import { saveAs } from "file-saver";
-
 // 1. npm i file-saver
 // 2. npm i --save-dev @types/file-saver
 
+var FileSaver = require("file-saver");
+
 export function saveFileAsJson(blob: Blob, name: string) {
-  saveAs(blob, name);
+  FileSaver.saveAs(blob, name);
 }
