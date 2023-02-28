c653f2b3a7fd433003f6da860cfe64e2da6ee4d9
LinkunGao <gaolinkun123@gmail.com>
2023-03-01 10:41:19 +1300

    update switch nrrd_tool eraser size response speed

--- src/index.ts
+++ src/index.ts
@@ -36,10 +36,10 @@ import {
 
 import "./css/style.css";
 
-export const REVISION = "v1.13.24";
+export const REVISION = "v1.13.25";
 
 console.log(
-  "%cCopper3D Visualisation %cBeta:v1.13.24",
+  "%cCopper3D Visualisation %cBeta:v1.13.25",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
