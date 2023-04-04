086a58543e03a3c7b59998a7f322dfa56aa6dc00
LinkunGao <gaolinkun123@gmail.com>
2023-04-04 15:22:42 +1200

    update nrrd loader plugin, and its relevent functions

--- src/index.ts
+++ src/index.ts
@@ -36,10 +36,10 @@ import {
 
 import "./css/style.css";
 
-export const REVISION = "v1.13.33";
+export const REVISION = "v1.14.0";
 
 console.log(
-  "%cCopper3D Visualisation %cBeta:v1.13.33",
+  "%cCopper3D Visualisation %cBeta:v1.14.0",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
