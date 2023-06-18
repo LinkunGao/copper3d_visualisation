b1e4d29301d0affcf1dc4f4136243aaaa11784e4
LinkunGao <gaolinkun123@gmail.com>
2023-06-19 10:36:44 +1200

    docs

--- src/index.ts
+++ src/index.ts
@@ -40,10 +40,10 @@ import {
 
 import "./css/style.css";
 
-export const REVISION = "v1.15.14";
+export const REVISION = "v1.15.15";
 
 console.log(
-  "%cCopper3D Visualisation %cBeta:v1.15.14",
+  "%cCopper3D Visualisation %cBeta:v1.15.15",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
