095f5a930942ef5a266ae08400d772d72eeaf04b
LinkunGao <gaolinkun123@gmail.com>
2023-11-05 13:12:41 +1300

    fixed cursor icon issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'LinkunGao'
 
 # The full version, including alpha/beta/rc tags
 
-release = 'v2.0.1'
+release = 'v2.0.2'
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -44,10 +44,10 @@ import { IPaintImage } from "./Utils/segmentation/coreTools/coreType";
 
 import "./css/style.css";
 
-export const REVISION = "v2.0.1";
+export const REVISION = "v2.0.2";
 
 console.log(
-  "%cCopper3D Visualisation %cBeta:v2.0.1",
+  "%cCopper3D Visualisation %cBeta:v2.0.2",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#f50a25"
 );
